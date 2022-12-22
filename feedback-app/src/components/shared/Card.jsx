90fcba17c56fa89a55522b47946912ace60f0df9
Chirag Khatri <chiragkhatri2001@gmail.com>
2022-12-23 03:52:41 +0530

    conditional function and conditional styling for card

--- feedback-app/src/components/shared/Card.jsx
+++ feedback-app/src/components/shared/Card.jsx
@@ -1,5 +1,16 @@
-function Card({ children }) {
-  return <div className="card">{children}</div>;
+function Card({ children, reverse }) {
+  //   return <div className={`card ${reverse && "reverse"}`}>{children}</div>; //conditional function
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundColor: reverse ? "rgba(0,0,0,0.4)" : "#fff",
+        color: reverse ? "#fff" : "#000",
+      }}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default Card;
