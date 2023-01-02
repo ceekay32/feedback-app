15ad21195c277ba2b3ddd709f81d8028cb939ec1
Chirag Khatri <chiragkhatri2001@gmail.com>
2023-01-02 23:58:06 +0530

    validation

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -4,7 +4,20 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setbtnDisabled] = useState("");
+  const [Message, setMessage] = useState("");
+
   const handleTextChange = (e) => {
+    if (text === "") {
+      setbtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setbtnDisabled(true);
+      setMessage("Text must be at least 10 characters");
+    } else {
+      setbtnDisabled(false);
+      setMessage(null);
+    }
     setText(e.target.value);
   };
   return (
@@ -21,8 +34,11 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className="message">{Message}</div>}
       </form>
     </Card>
   );
